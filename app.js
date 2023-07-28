bac4a68dc5c02436e307b15121a5bc1ad5d5e08d
natybastazini <natybastazini7@gmail.com>
2023-07-28 12:27:20 -0300

    git add .

---
+++ app.js
@@ -0,0 +1,13 @@
+// Permite exibir uma mensagem no terminal.
+console.log("Testando o Node.JS");
+
+// Criar variável.
+var nome = "Natália";
+
+//Para concatenar texto e variável podemos utilizar o + ou a forma reduzida.
+console.log("O nome do usuário é: " + nome);
+console.log(`O nomedo usuário é: ${nome}`)
+
+//typeof() - permite especificar qual o tipo de dados de uma variável ou objeto.
+console.log(typeof(nome))
+
